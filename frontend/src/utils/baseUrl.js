8af8f5178142a4f76b87603d337c46fb6d3fc25e
Aidan Lee <aidankglee@googlemail.com>
2022-04-22 13:17:27 +0100

    Altered node env variables.

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,3 +1,4 @@
 const env = process.env.NODE_ENV
+console.log(env)
 const baseUrl = env === 'development' ? 'https://localhost:3000' : 'https://i-commerce-uk.herokuapp.com';
 export default baseUrl;
