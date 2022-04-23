5a8f6c464c9b065f9c078fbb53853de7ad229870
Aidan Lee <aidankglee@googlemail.com>
2022-04-23 22:29:45 +0100

    Fixed bag and saved items pages.

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV
+const env = 'production'
 console.log(env)
 const baseUrl = env === 'development' ? 'https://localhost:3000' : 'https://i-commerce-uk.herokuapp.com';
 export default baseUrl;
